063cd42275577d5b1381f97b97d395cb48138fb6
Arthur zagré <zagrarthur123@gmail.com>
2020-04-06 23:47:43 -0400

    finishing building button up in Restaurant Details

--- MiniProjetKungry/app/src/main/java/ca/ulaval/ima/mp/RestaurantDetails.java
+++ MiniProjetKungry/app/src/main/java/ca/ulaval/ima/mp/RestaurantDetails.java
@@ -3,6 +3,9 @@ package ca.ulaval.ima.mp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import ca.ulaval.ima.mp.ui.fragmentpackage.RestaurantDescription;
 
@@ -12,10 +15,26 @@ public class RestaurantDetails extends AppCompatActivity implements RestaurantDe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_details);
+
+        ImageView imageView = findViewById(R.id.img1);
+        imageView.bringToFront();
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
     }
 
     @Override
     public void showDetails() {
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
 
     }
 }
